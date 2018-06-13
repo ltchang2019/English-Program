b59d7813a7c41989e98f6753641af203e861c673
Luke Tchang <ltchang2019@francisparker.org>
2018-06-12 21:45:00 -0700

    readjusted login system and database to fit first last name model and redirect after login goes to finalproject.html

--- registration/register.php
+++ registration/register.php
@@ -13,13 +13,20 @@
   <form method="post" action="register.php">
     <?php include('errors.php'); ?>
     <div class="input-group">
+      <label>First Name</label>
+      <input type="firstName" name="firstName" value="<?php echo $firstName; ?>">
+    </div>
+
+    <div class="input-group">
+      <label>Last Name</label>
+      <input type="lastName" name="lastName" value="<?php echo $lastName; ?>">
+    </div>
+
+    <div class="input-group">
       <label>Username</label>
       <input type="text" name="username" value="<?php echo $username; ?>">
     </div>
-    <div class="input-group">
-      <label>Email</label>
-      <input type="email" name="email" value="<?php echo $email; ?>">
-    </div>
+    
     <div class="input-group">
       <label>Password</label>
       <input type="password" name="password_1">
