88e67168931b6e89c34ea7d240f3d627decbae0b
Luke Tchang <ltchang2019@francisparker.org>
2018-06-12 21:31:43 -0700

    THIS IS THE FUNCTIONAL LOGIN SYSTEM THAT STILL DOESN'T HAVE FIRST AND LAST NAME

--- registration/login.php
+++ registration/login.php
@@ -7,25 +7,25 @@
 </head>
 <body>
   <div class="header">
-  	<h2>Login</h2>
+    <h2>Login</h2>
   </div>
-	 
-  <form method="post" action="finalproject.html">
-  	<?php include('errors.php'); ?>
-  	<div class="input-group">
-  		<label>Username</label>
-  		<input type="text" name="username" >
-  	</div>
-  	<div class="input-group">
-  		<label>Password</label>
-  		<input type="password" name="password">
-  	</div>
-  	<div class="input-group">
-  		<button type="submit" class="btn" name="login_user">Login</button>
-  	</div>
-  	<p>
-  		Not yet registered? <a href="register.php">Create an Account</a>
-  	</p>
+   
+  <form method="post" action="login.php">
+    <?php include('errors.php'); ?>
+    <div class="input-group">
+      <label>Username</label>
+      <input type="text" name="username" >
+    </div>
+    <div class="input-group">
+      <label>Password</label>
+      <input type="password" name="password">
+    </div>
+    <div class="input-group">
+      <button type="submit" class="btn" name="login_user">Login</button>
+    </div>
+    <p>
+      Not yet a member? <a href="register.php">Sign up</a>
+    </p>
   </form>
 </body>
 </html>
--- registration/server.php
+++ registration/server.php
@@ -23,7 +23,7 @@ if (isset($_POST['reg_user'])) {
   if (empty($email)) { array_push($errors, "Email is required"); }
   if (empty($password_1)) { array_push($errors, "Password is required"); }
   if ($password_1 != $password_2) {
-	array_push($errors, "The two passwords do not match");
+  array_push($errors, "The two passwords do not match");
   }
 
   // first check the database to make sure 
@@ -44,14 +44,14 @@ if (isset($_POST['reg_user'])) {
 
   // Finally, register user if there are no errors in the form
   if (count($errors) == 0) {
-  	$password = md5($password_1);//encrypt the password before saving in the database
+    $password = md5($password_1);//encrypt the password before saving in the database
 
-  	$query = "INSERT INTO users (username, email, password) 
-  			  VALUES('$username', '$email', '$password')";
-  	mysqli_query($db, $query);
-  	$_SESSION['username'] = $username;
-  	$_SESSION['success'] = "You are now logged in";
-  	header('location: index.php');
+    $query = "INSERT INTO users (username, email, password) 
+          VALUES('$username', '$email', '$password')";
+    mysqli_query($db, $query);
+    $_SESSION['username'] = $username;
+    $_SESSION['success'] = "You are now logged in";
+    header('location: index.php');
   }
 }
 
