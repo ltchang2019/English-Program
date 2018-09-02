f106b8d645cef5cad997cd6526dee9f0316ef424
Luke Tchang <ltchang2019@francisparker.org>
2018-09-01 20:20:47 -0700

    added browse grammar

--- LookUpUser.php
+++
@@ -1,6 +0,0 @@
-
-
-<form action="welcome.php" method="post">
-Username: <input type="text" name="username"><br>
-<input type="submit">
-</form>
--- somePage.php
+++
@@ -1,8 +0,0 @@
-<ul>
-<?php
-foreach( $_GET as $k => $v )
-echo "<li>" . $k . " has value " . $v . "</li>";
-?>
-</ul>
-Enter stuff in url
-
